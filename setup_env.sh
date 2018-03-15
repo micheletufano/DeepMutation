47b99fbe9429cbc6757e2a82a62d5e78d8026419
Jason Kimko <jskimko@email.wm.edu>
2018-03-15 03:00:51 -0400

    Update script.
        - Change to tensorflow 1.3
        - Add user-level install flag
        - Add additional troubleshooting.
        - Change shell prompts

--- setup_env.sh
+++ setup_env.sh
@@ -8,14 +8,14 @@ command -v python >/dev/null 2>&1 ||
 command -v pip >/dev/null 2>&1 || 
     { echo >&2 "Please install pip."; exit 1; }
 
-# Check tensorflow 1.0
+# Check tensorflow 1.3
 echo "Checking tensorflow... "
 tf_ver=`pip show tensorflow | grep Version | awk '{print $2}' \
                                            | sed 's/\(.*\..*\)\..*/\1/'`
 
-if [ -z "$tf_ver" ] || [ ! "$tf_ver" = "1.0" ]; then
-    echo >&2 "Please install tensorflow 1.0 using:
-# pip install tensorflow==1.0"
+if [ -z "$tf_ver" ] || [ ! "$tf_ver" = "1.3" ]; then
+    echo >&2 "Please install tensorflow 1.3 using:
+> pip install [--user] tensorflow==1.3"
     exit 1
 fi
 
@@ -25,18 +25,20 @@ s2s_ver=`pip show seq2seq | grep Version | awk '{print $2}'`
 
 if [ -z "$s2s_ver" ] || [ ! "$s2s_ver" = "0.1" ]; then
     echo >&2 "Please install seq2seq 0.1 using:
-# git clone https://github.com/google/seq2seq.git
-# cd seq2seq
-# pip install -e ."
+> git clone https://github.com/google/seq2seq.git
+> cd seq2seq
+> pip install [--user] -e ."
     exit 1
 fi
 
 # Test seq2seq
 echo "
 To test seq2seq, run the following:
-$ python -m unittest seq2seq.test.pipeline_test
+> python -m unittest seq2seq.test.pipeline_test
 
 An 'OK' indicates a successful test.
 
-Please refer to https://github.com/google/seq2seq/issues/285 for:
-    ImportError: cannot import name 'bernoulli'"
+If the seq2seq module is not found, please check that pip and python are
+on the same version.
+
+Please refer to https://github.com/google/seq2seq/issues/285 for ImportErrors."
