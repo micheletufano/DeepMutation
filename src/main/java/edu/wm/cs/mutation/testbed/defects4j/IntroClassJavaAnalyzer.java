0ad3d1eabc1ce1a52ef96f545f666d12b48a1c84
Michele Tufano <mtufano@email.wm.edu>
2018-04-06 16:58:28 -0400

    New testbed: IntroClassJava

---
+++ src/main/java/edu/wm/cs/mutation/testbed/defects4j/IntroClassJavaAnalyzer.java
@@ -0,0 +1,139 @@
+package edu.wm.cs.mutation.testbed.defects4j;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import edu.wm.cs.mutation.abstractor.MethodAbstractor;
+import edu.wm.cs.mutation.abstractor.MethodTranslator;
+import edu.wm.cs.mutation.extractor.MethodExtractor;
+import edu.wm.cs.mutation.io.IOHandler;
+import edu.wm.cs.mutation.mutator.MethodMutator;
+import edu.wm.cs.mutation.tester.MutantTester;
+import spoon.reflect.declaration.CtMethod;
+
+public class IntroClassJavaAnalyzer {
+
+	private static final String SRC = "src/main/java/introclassJava/";
+	private static final String LIB_PATH = null;
+	private static final int complianceLvl = 7;
+	private static final boolean compiled = false;
+	
+	private static final String EXEC_METHOD = "exec";
+
+
+	public static void analyze(String programsRoot, String mutationModelRoot, String out, String idiomPath, String mvnBin, int beamWidth) {
+
+		//Mutation models settings
+		List<String> modelPaths = IOHandler.listDirectoriesPaths(mutationModelRoot);
+
+		//MutantTester settings
+		MutantTester.setCompileCmd(mvnBin, "compile");
+		MutantTester.setTestCmd(mvnBin, "test");
+		MutantTester.setCompileFailString("FAIL");
+		MutantTester.setTestFailString("ailure");
+		MutantTester.useBaseline(false);
+		MutantTester.setParallel(false);
+
+		//MethodMutator settings
+		MethodMutator.useBeams(true);
+		MethodMutator.setNumBeams(beamWidth);
+		MethodMutator.setPython("python3");
+		
+		MethodAbstractor.setInputMode(true);
+		
+		PrintStream console = System.out;
+		
+		List<String> programs = IOHandler.listDirectoriesPaths(programsRoot);
+	
+		for(String program : programs) {
+
+			List<String> versions = IOHandler.listDirectoriesPaths(program);
+
+			for(String programVersion : versions) {
+
+				String projPath = programVersion;
+				String srcPath = projPath + SRC;
+			
+				String[] pathInfo = programVersion.split("/");
+				String versionName = pathInfo[pathInfo.length-1];
+				String programID = pathInfo[pathInfo.length-2];
+				String programName = pathInfo[pathInfo.length-3];
+				String outPath = out + "/" + programName + "/" + programID + "/" + versionName + "/";
+				String outProjectLog = out + "/log/" + programName + "/" + programID;
+				IOHandler.createDirectories(outProjectLog);
+				
+				System.out.println(programName + " - " + programID + " - " + versionName);
+
+				String logFile = outProjectLog + "/" + versionName + ".log";
+				IOHandler.setOutputStream(logFile);
+				
+				
+				//Extract all methods
+				MethodExtractor.extractMethods(projPath, srcPath, LIB_PATH, complianceLvl, compiled, null);
+				IOHandler.writeMethods(outPath, MethodExtractor.getRawMethodsMap(), false);
+				
+				//Find exec method
+				findExecMethod();
+				
+				//Abstract exec method
+				MethodAbstractor.abstractMethods(MethodExtractor.getRawMethodsMap(), idiomPath);
+				IOHandler.writeMethods(outPath, MethodAbstractor.getAbstractedMethods(), true);
+				IOHandler.writeMappings(outPath, MethodAbstractor.getMappings());
+
+				MethodMutator.mutateMethods(outPath, MethodAbstractor.getAbstractedMethods(), modelPaths);
+				IOHandler.writeMutants(outPath, MethodMutator.getMutantsMap(), modelPaths, true);
+
+				MethodTranslator.translateMethods(MethodMutator.getMutantsMap(), MethodAbstractor.getMappings(), modelPaths);
+				IOHandler.writeMutants(outPath, MethodTranslator.getTranslatedMutantsMap(), modelPaths, false);
+
+				IOHandler.createMutantFiles(outPath, MethodTranslator.getTranslatedMutantsMap(),
+						MethodExtractor.getMethods(), modelPaths);
+
+				MutantTester.testMutants(projPath, MethodTranslator.getTranslatedMutantsMap(),
+						MethodExtractor.getMethods(), modelPaths);
+//				IOHandler.writeBaseline(outPath, MutantTester.getCompileBaseline(), "compile");
+//				IOHandler.writeBaseline(outPath, MutantTester.getTestBaseline(), "test");
+				IOHandler.writeLogs(outPath, MutantTester.getCompileLogs(), modelPaths, "compile");
+				IOHandler.writeLogs(outPath, MutantTester.getTestLogs(), modelPaths, "test");
+				IOHandler.writeResults(outPath, MutantTester.getCompilable(), modelPaths, "compile");
+				IOHandler.writeResults(outPath, MutantTester.getSuccessful(), modelPaths, "test");
+
+
+			}
+			
+			System.setOut(console);
+
+		}
+		
+		
+
+	}
+	
+	
+	private static void findExecMethod() {
+		LinkedHashMap<String, String> execRawMethod = new LinkedHashMap<>();
+		LinkedHashMap<String, String> extractedRawMethods = MethodExtractor.getRawMethodsMap();
+
+		for(String key : extractedRawMethods.keySet()) {
+			if(key.contains(EXEC_METHOD)){
+				System.out.println("Found method exec!");
+				execRawMethod.put(key, extractedRawMethods.get(key));
+			}
+		}
+		
+		List<CtMethod> execMethod = new ArrayList<>();
+		List<CtMethod> extractedMethods = MethodExtractor.getMethods();
+		for(CtMethod m : extractedMethods) {
+			if(m.getSimpleName().contains(EXEC_METHOD)) {
+				execMethod.add(m);
+			}
+		}
+		
+		MethodExtractor.setRawMethodsMap(execRawMethod);
+		MethodExtractor.setMethods(execMethod);
+	}
+	
+
+}
---
+++ src/test/java/edu/wm/cs/mutation/testbed/defects4j/IntroClassJavaAnalyzerTest.java
@@ -0,0 +1,28 @@
+package edu.wm.cs.mutation.testbed.defects4j;
+
+public class IntroClassJavaAnalyzerTest {
+
+	public static void main(String[] args) {
+
+//		String programsRoot = "/scratch/mtufano.scratch/projects/learningFixes/IntroClassJava/IntroClassJava/dataset/checksum/";
+//		String mutationModelRoot = "/scratch/mtufano.scratch/tmp/IntroClassJava_Test/models/";
+//		String out = "/scratch/mtufano.scratch/tmp/IntroClassJava_Test/";
+//		String idiomPath = "/scratch/mtufano.scratch/projects/learningFixes/IntroClassJava/idioms.csv";
+//		String mvnBin = "";
+//		int beamWidth = 10;
+		
+		String programsRoot = args[0];
+		String mutationModelRoot = args[1];
+		String out = args[2];
+		String idiomPath = args[3];
+		String mvnBin = args[4];
+		int beamWidth = Integer.parseInt(args[5]);		
+		
+		
+		
+		IntroClassJavaAnalyzer.analyze(programsRoot, mutationModelRoot, out, idiomPath, mvnBin, beamWidth);
+		
+		
+	}
+
+}
