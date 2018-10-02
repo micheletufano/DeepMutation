4e1b2724b69e5dffd8effac3b354876087daf4ef
Steve Kommrusch <steve.kommrusch@gmail.com>
2018-10-02 11:16:37 +0200

    Improved error checks and messages in setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# Check that we are running this (not sourcing) so that exit clauses are safe
+if [[ "$(basename -- "$0")" != "setup_env.sh" ]]; then
+    echo "Don't source setup_env.sh, run it"
+# The rest of program is within the else close
+else
+
 # Check python
-command -v python >/dev/null 2>&1 || 
-    { echo >&2 "Please install python."; exit 1; }
+command -v python >/dev/null 2>&1 && python --version 2>&1 | grep -q " 3.[0-6]" || 
+    { echo >&2 "Please install python version 3.0.* through 3.6.*."; exit 1; }
 
 # Check pip
 command -v pip >/dev/null 2>&1 || 
@@ -15,7 +21,7 @@ tf_ver=`pip show tensorflow | grep Version | awk '{print $2}' \
 
 if [ -z "$tf_ver" ] || [ ! "$tf_ver" = "1.3" ]; then
     echo >&2 "Please install tensorflow 1.3 using:
-> pip install [--user] tensorflow==1.3"
+> pip install [--user] tensorflow==1.3.0"
     exit 1
 fi
 
@@ -44,10 +50,19 @@ Please test seq2seq manually using:
 An 'OK' indicates a successful test.
 
 If the seq2seq module is not found, please check that pip and python are
-on the same version.
+on the correct version.
+
+A common issue related to helper.py can be addressed by following these steps:
+  emacs seq2seq/contrib/seq2seq/helper.py
+  Replace the import lines for bernoulli and categorical with these:
+    from tensorflow.contrib.distributions import Bernoulli
+    from tensorflow.contrib.distributions import Categorical
 
 Please refer to https://github.com/google/seq2seq/issues/285 for ImportErrors."
     exit 1
 else 
     echo "Success."
+fi
+
+# End of 'source' check
 fi
